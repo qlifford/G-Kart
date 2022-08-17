99eab2b52304c95e10ba81db1f809f1210613345
pass <otienocee@gmail.com>
2022-08-17 08:39:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gkart/activity/PaymentActivity.kt
@@ -0,0 +1,29 @@
+package com.example.gkart.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.gkart.MainActivity
+import com.example.gkart.R
+import com.example.gkart.databinding.ActivityPaymentBinding
+
+class PaymentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPaymentBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPaymentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCancel.setOnClickListener {
+            Toast.makeText(this, "Payment Canceled", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        binding.btnPay.setOnClickListener {
+            Toast.makeText(this, "Thank you for shopping with us", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
